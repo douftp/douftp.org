4d31aa647c675f660bd9a58918c31fd55466cfca
Jackson Dou <doudoudzj@qq.com>
2021-08-31 00:32:51 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,22 @@
 module.exports = {
-    title: 'DOUFTP',
+    title: 'DouFTP',
     description: 'DouFTP 桌面FTP应用程序',
+    head: [
+        ['meta', { name: 'renderer', content: 'webkit' }],
+        ['meta', { name: 'force-rendering', content: 'webkit' }],
+        ['meta', { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge, chrome=1' }],
+        ['meta', { name: 'author', content: 'CROGRAM' }],
+        ['meta', { name: 'copyright', content: 'CROGRAM 程江开源' }],
+        ['meta', { name: 'keywords', content: 'douftp, 开源, FTP, DouFTP 桌面FTP应用程序' }],
+        ['meta', { name: 'baidu-site-verification', content: 'code-7Wz1TUiNJj' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ],
     themeConfig: {
         nav: [
             { text: 'Home', link: '/' },
-            { text: 'Client', link: '/client' },
-            { text: 'Server', link: '/server' },
+            { text: '客户端', link: '/client' },
+            { text: '服务端', link: '/server' },
             // {
             //     text: 'Languages',
             //     ariaLabel: 'Language Menu',
